34f490ec2462e209430b194ad89c837c8caf364e
Ararat777 <danielpogosyan18@gmail.com>
2018-01-31 00:27:32 +0200

    heroku

--- app/models/invited_user.rb
+++ app/models/invited_user.rb
@@ -5,8 +5,9 @@ class InvitedUser < ApplicationRecord
   validates :link,
             presence: {message: "Поле не должно быть пустым"},
             format: {with: /\A[^А-Яа-я]+\z/,message: "Допускаются только латинские буквы"},
-            format: {with: /\Ahttps\:\/\/www\.facebook\.com.+\z/,message: "Ссылка должна быть как в примере"}, if: :soc_network_is_fb?,
             uniqueness: {scope: :link,message: "Такой юзер уже есть"}
+  validates format: {with: /\Ahttps\:\/\/www\.facebook\.com.+\z/,message: "Ссылка должна быть как в примере"}, if: :soc_network_is_fb?
+  
   
   def soc_network_is_fb?
     SocNetwork.find(soc_network_id).title == "Facebook"
