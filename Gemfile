bf77ef38fa8ca863ffc12e6c09655ebc5bee67cc
Ararat777 <danielpogosyan18@gmail.com>
2018-01-30 23:32:54 +0200

    heroku

--- Gemfile
+++ Gemfile
@@ -45,6 +45,10 @@ group :development, :test do
   gem 'selenium-webdriver'
 end
 
+group :production do
+  gem "pg"
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
@@ -56,3 +60,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+ruby "2.3.1p112"
--- app/models/invited_user.rb
+++ app/models/invited_user.rb
@@ -5,6 +5,11 @@ class InvitedUser < ApplicationRecord
   validates :link,
             presence: {message: "Поле не должно быть пустым"},
             format: {with: /\A[^А-Яа-я]+\z/,message: "Допускаются только латинские буквы"},
+            format: {with: /\Ahttps\:\/\/www\.facebook\.com.+\z/,message: "Ссылка должна быть как в примере"}, if: :soc_network_is_fb?,
             uniqueness: {scope: :link,message: "Такой юзер уже есть"}
+  
+  def soc_network_is_fb?
+    SocNetwork.find(soc_network_id).title == "Facebook"
+  end
             
 end
