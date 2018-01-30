ddb35f8dcd70949f800a159ad674129b73dd3ea3
Ararat777 <danielpogosyan18@gmail.com>
2018-01-31 00:24:00 +0200

    heroku

--- app/models/soc_user_link.rb
+++ app/models/soc_user_link.rb
@@ -4,6 +4,6 @@ class SocUserLink < ApplicationRecord
   enum :status => [:pending,:confirmed,:unconfirmed]
   validates :link,
             presence: {message: "Поле не должно быть пустым"},
-            uniqueness: {scope: :link,message: "Такой юзер уже есть"},unless: :user_has_been_unconfirmed?,
+            uniqueness: {scope: :link,message: "Такой юзер уже есть"},
             format: {with: /\A[^А-Яа-я]+\z/,message: "Допускаются только латинские буквы"}
 end
